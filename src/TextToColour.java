25e50844472f7ab80356933aa3cf45ff93a3b4dd
svladcristian <svladcristian@gmail.com>
2014-06-28 16:28:08 +0300

    Determined resulting image size.
    Determined the total number and distribution of pixels.

--- src/TextToColour.java
+++ src/TextToColour.java
@@ -1,17 +1,32 @@
-/**
- * @(#)TextToColour.java
- *
- * TextToColour application
- *
- * @author 
- * @version 1.00 2014/6/28
- */
- 
+import java.util.Scanner;
+
 public class TextToColour {
-    
+	
     public static void main(String[] args) {
     	
-    	// TODO, add your application code
-    	System.out.println("Hello World!");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("m=");
+		int m = sc.nextInt();
+		
+		int n;
+		if (m%3 == 0) n = m/3;
+		else n = m/3 + 1;
+		System.out.println("n=" + n);
+
+		int i = 0, j = 1, k = 1;
+		do {
+			do {
+				k++;
+				if (n%k == 0) {
+					j = n/k; i = k;
+				}
+			}while (i < j);
+			if (j == 1) {
+				i = 0; j = 1; k = 1;
+				n++;
+			}
+		}while (i < j);
+		System.out.println(n + "=" + i + "*" + j);
+		
     }
 }

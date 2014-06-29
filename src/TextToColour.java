bad62b126f86d7b7670c2ed1014bea75e20c4988
svladcristian <svladcristian@gmail.com>
2014-06-29 17:15:18 +0300

    Read from file.
    Gathered all text together and determined with threshold the optimum
    size of the resulting image.

--- src/TextToColour.java
+++ src/TextToColour.java
@@ -1,32 +1,54 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class TextToColour {
 	
     public static void main(String[] args) {
     	
-		Scanner sc = new Scanner(System.in);
-		System.out.print("m=");
-		int m = sc.nextInt();
-		
+    	String FILENAME = "data.txt";
+    	String text = new String();
+    	try {
+    		Scanner scanner = new Scanner(new File(FILENAME));
+    		StringBuilder stringBuilder = new StringBuilder();
+    		stringBuilder.append(scanner.nextLine());
+    		String newLine = System.getProperty("line.separator");
+	    	while (scanner.hasNextLine()) {
+	    		stringBuilder.append(newLine + scanner.nextLine());
+	    	}
+	    	text = stringBuilder.toString();
+    	}catch(FileNotFoundException ex) {
+    		ex.printStackTrace();
+    	}
+    	System.out.println(text);
+
+		int m = text.length();
 		int n;
 		if (m%3 == 0) n = m/3;
 		else n = m/3 + 1;
 		System.out.println("n=" + n);
 
-		int i = 0, j = 1, k = 1;
-		do {
+		int THRESHOLD = 5;
+		int sizeThreshold = THRESHOLD % n;
+		int ni = 1, nj = 1, minDiff = n;
+		for (int i = 0; i < sizeThreshold; i++) {
+			int tempi = 0, tempj = 1, k = 1;
 			do {
 				k++;
 				if (n%k == 0) {
-					j = n/k; i = k;
+					tempj = n/k;
+					tempi = k;
 				}
-			}while (i < j);
-			if (j == 1) {
-				i = 0; j = 1; k = 1;
-				n++;
+			}while (tempi < tempj);
+			int tempDiff = tempi - tempj; 
+			if (tempDiff < minDiff) {
+				minDiff = tempDiff;
+				ni = tempi;
+				nj = tempj;
 			}
-		}while (i < j);
-		System.out.println(n + "=" + i + "*" + j);
-		
+			n++;
+		}
+		n = ni*nj;
+		System.out.println(n + "=" + ni + "*" + nj);
     }
 }

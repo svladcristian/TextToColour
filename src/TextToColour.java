2f01fd0bb99b6b36821dd7850d50e429379614ca
svladcristian <svladcristian@gmail.com>
2014-07-01 13:07:24 +0300

    Determined pixel values.
    Went through text and the remaining filler pixels.

--- src/TextToColour.java
+++ src/TextToColour.java
@@ -26,7 +26,9 @@ public class TextToColour {
 		int n;
 		if (m%3 == 0) n = m/3;
 		else n = m/3 + 1;
-		System.out.println("n=" + n);
+		int roundm = n;
+		System.out.println("m=" + m);
+		System.out.println("round m/3=" + roundm);
 
 		int THRESHOLD = 5;
 		int sizeThreshold = THRESHOLD % n;
@@ -49,6 +51,45 @@ public class TextToColour {
 			n++;
 		}
 		n = ni*nj;
-		System.out.println(n + "=" + ni + "*" + nj);
+		System.out.println("n=" + n + "=" + ni + "*" + nj);
+	
+		/*
+		 * Determining pixel values, obtained strictly from text characters,
+		 * with at most 2 rgb value fillers, to complete the last pixel.
+		 * - roundm reached
+		 */
+		int np = 0;
+		int i;
+		for (i = 0; i+3 < m; i += 3) {
+			int red = (int) text.charAt(i);
+			int green = (int) text.charAt(i+1);
+			int blue = (int) text.charAt(i+2);
+			np++;
+		}
+		if (m - i > 0) {
+			int red = (int) text.charAt(i);
+			if (m - i > 1) {
+				int green = (int) text.charAt(i+1);
+			}
+			else {
+				int green = 0;
+			}
+			int blue = 0;
+			np++;
+		}
+		System.out.println("Number of pixels obtained: " + np + " - " + "Number of pixels expected: " + roundm);
+		
+		/*
+		 * Filling remaining pixels to completion.
+		 * - n reached
+		 */
+		for (i = roundm; i < n; i++) {
+			int red = 0;
+			int green = 0;
+			int blue = 0;
+			np++;
+		}
+		System.out.println("Number of pixels obtained: " + np + " - " + "Number of pixels expected: " + n);
+		
     }
 }
